8906415fd3b011ed5bb1ad9aaa849e75bc8a9563
haseeb khan <haseebkhanbettani@gmail.com>
2025-08-18 19:27:06 +0500

    Initial commit

---
+++ cpp/main.cpp
@@ -0,0 +1,10 @@
+#include "../../Cmake/src/cpp/adder.h"
+#include <iostream>
+
+
+int main(){
+
+	std::cout <<  haseebMath::add(2,3);
+
+	return 0;
+}
---
+++ p1/src/cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "sklksjfn" ;
+    cout << "sadasd";
+    cout <<"dasdsad";
+    return 0;
+}
