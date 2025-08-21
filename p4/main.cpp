c6588a6497719bcb43df7c5d60b93f112720ce7a
haseeb khan <haseebkhanbettani@gmail.com>
2025-08-22 04:46:26 +0500

    git submodule update --init --recursive.
    Signed-off-by: haseeb khan <haseebkhanbettani@gmail.com>

--- p4/main.cpp
+++ p4/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include "Adder/adder.h"
+#include <GLFW/glfw3.h>
 using namespace std;
 
 int main()
 {
     cout << "3 + 4 = " << ::haseebMath::add(3,4);
 
+    GLFWwindow *window;
+    
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
 }
