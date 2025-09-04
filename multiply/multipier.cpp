308332714ffdbfb73d1bfde004c3cb56ed71eb21
haseeb khan <haseebkhanbettani@gmail.com>
2025-09-04 13:08:16 +0500

    Added another subproject (math/) inside Project. this is called In-Tree Build.
    Signed-off-by: haseeb khan <haseebkhanbettani@gmail.com>

---
+++ multiply/multipier.cpp
@@ -0,0 +1,14 @@
+#include "multipier.hpp"
+#include <iostream>
+using namespace std;
+int math1::multi(int x, int y)
+{
+    cout << "Library is used." <<endl;
+    return (x * y);
+}
+
+double math1::multi(double x, double y)
+{
+    cout << "Library is used." <<endl;
+    return (x * y);
+}
