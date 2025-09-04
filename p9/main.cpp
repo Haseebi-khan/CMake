3979bfd7a9d82f68bcf069f1a8aee61148bc330f
haseeb khan <haseebkhanbettani@gmail.com>
2025-09-04 18:34:00 +0500

    Added another Static subproject (math/) inside Project. is called In-Tree Build.
    Signed-off-by: haseeb khan <haseebkhanbettani@gmail.com>

---
+++ p9/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <multipier.hpp>
+
+using namespace std;
+
+int main()
+{
+    cout << "Haseeb Khan." << endl;    
+
+    cout << "Multiplixer: " << ::math1::multi(3,4) << endl;
+
+    return 0;
+}
